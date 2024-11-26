f4bddbbe341c221c41d8bde86a9702550513cb90
Hrytsenko Artem <hrytsenko.artem@lll.kpi.ua>
2024-11-26 19:19:03 +0200

    feat: add Data folder and DbContext class

---
+++ UKRWalks.API/Data/UKRWalksDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UKRWalks.API.Models.Domain;
+
+namespace UKRWalks.API.Data
+{
+    public class UKRWalksDbContext : DbContext
+    {
+        public UKRWalksDbContext(DbContextOptions dbContextOptions) : base (dbContextOptions)
+        {
+
+        }
+
+        public DbSet<Difficulty> Difficulties { get; set; }
+        public DbSet<Region> Regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+    }
+}
