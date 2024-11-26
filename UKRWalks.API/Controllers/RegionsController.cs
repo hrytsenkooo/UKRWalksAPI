7776a97b3db8b42529cd61593558d4120cbb4d44
Hrytsenko Artem <hrytsenko.artem@lll.kpi.ua>
2024-11-26 21:47:06 +0200

    feat: added DTO folder and created new controller for Regions (CRUD)

---
+++ UKRWalks.API/Controllers/RegionsController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UKRWalks.API.Data;
+using UKRWalks.API.Models.Domain;
+using UKRWalks.API.Models.DTO;
+
+namespace UKRWalks.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionsController : ControllerBase
+    {
+        private readonly UKRWalksDbContext dbContext;
+
+        public RegionsController(UKRWalksDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var regions = dbContext.Regions.ToList();
+            var regionsDto = new List<RegionDto>();
+            foreach (var region in regions)
+            {
+                regionsDto.Add(new RegionDto()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    RegionImageUrl = region.RegionImageUrl
+                });
+            }
+            return Ok(regionsDto);
+        }
+
+        [HttpGet]
+        [Route("{id}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+
+            if (region == null) return NotFound();
+
+            var regionDto = new RegionDto
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+            return Ok(regionDto);
+        }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] AddRegionRequestDto addRegionRequestDto)
+        {
+            var region = new Region
+            {
+                Code = addRegionRequestDto.Code,
+                Name = addRegionRequestDto.Name,
+                RegionImageUrl = addRegionRequestDto.RegionImageUrl
+            };
+
+            dbContext.Regions.Add(region);
+            dbContext.SaveChanges();
+
+            var regionDto = new RegionDto
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+            return CreatedAtAction(nameof(GetById), new { id = region.Id }, regionDto);
+        }
+
+        [HttpPut]
+        [Route("{id:guid}")]
+        public IActionResult Update([FromRoute] Guid id, [FromBody] UpdateRegionRequestDto updateRegionRequestDto)
+        {
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (region == null) return NotFound();
+
+            region.Code = updateRegionRequestDto.Code;
+            region.Name = updateRegionRequestDto.Name;
+            region.RegionImageUrl = updateRegionRequestDto.RegionImageUrl;
+
+            dbContext.SaveChanges();
+
+            var regionDto = new RegionDto
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+            return Ok(regionDto);
+        }
+
+        [HttpDelete]
+        [Route("{id:guid}")]
+        public IActionResult Delete([FromRoute] Guid id)
+        {
+            var region = dbContext.Regions.FirstOrDefault(x => x.Id == id);
+            if (region == null) return NotFound();
+
+            dbContext.Regions.Remove(region);
+            dbContext.SaveChanges();
+
+            var regionDto = new RegionDto
+            {
+                Id = region.Id,
+                Code = region.Code,
+                Name = region.Name,
+                RegionImageUrl = region.RegionImageUrl
+            };
+
+            return Ok(regionDto);
+        }
+    }
+}
